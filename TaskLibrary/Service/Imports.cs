79f542274b9c19cc156b17b674e3817e9c5c50c6
NightFuryL <levodesskiy@gmail.com>
2026-03-23 13:06:45 +0200

    Add project files.

---
+++ TaskLibrary/Service/Imports.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TaskLibrary.Service;
+
+public static class Imports
+{
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, ExactSpelling = false)]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+    [DllImport("kernel32.dll")]
+    public static extern bool Beep(uint dwFreq, uint dwDuration);
+
+    [DllImport("user32.dll")]
+    public static extern bool MessageBeep(uint type);
+}
